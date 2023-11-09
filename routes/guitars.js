d5969295b38303640143bc981adf5f279aaaf726
Landon Swavey <s541663@nwmissouri.edu>
2023-11-08 21:11:30 -0600

    more debugging

--- routes/guitars.js
+++ routes/guitars.js
@@ -1,7 +1,7 @@
 var express = require('express');
-const guitar = controllers=require('../controllers/guitar');
+const guitar =require('../controllers/guitar');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', guitar_controllers.guitar_view_all_Page);
+router.get('/', guitar.guitar_view_all_Page);
 module.exports = router;
