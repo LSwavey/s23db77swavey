508fd97d6965424b2f8ef472e5f6dc1a9ea471cf
Landon Swavey <s541663@nwmissouri.edu>
2023-11-10 11:44:11 -0600

    good changes

--- controllers/guitar.js
+++ controllers/guitar.js
@@ -17,7 +17,27 @@ res.send('NOT IMPLEMENTED: Guitar delete DELETE ' + req.params.id);
 };
 // Handle Guitar update form on PUT.
 exports.guitar_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Guitar update PUT' + req.params.id);
+//res.send('NOT IMPLEMENTED: Guitar update PUT' + req.params.id);
+// Handle Guitar update form on PUT.
+exports.guitar_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Guitar.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.guitar_type)
+    toUpdate.guitar_type = req.body.guitar_type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.model) toUpdate.model = req.body.model;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 };
 
 // List of all Guitars
@@ -78,23 +98,3 @@ res.send(`{"error": document for id ${req.params.id} not found`);
 }
 };
 
-// Handle Guitar update form on PUT.
-exports.guitar_update_put = async function(req, res) {
-console.log(`update on id ${req.params.id} with body
-${JSON.stringify(req.body)}`)
-try {
-let toUpdate = await Guitar.findById( req.params.id)
-// Do updates of properties
-if(req.body.guitar_type)
-toUpdate.guitar_type = req.body.guitar_type;
-if(req.body.cost) toUpdate.cost = req.body.cost;
-if(req.body.model) toUpdate.model = req.body.model;
-let result = await toUpdate.save();
-console.log("Sucess " + result)
-res.send(result)
-} catch (err) {
-res.status(500)
-res.send(`{"error": ${err}: Update for id ${req.params.id}
-failed`);
-}
-};
